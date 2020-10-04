94f95061b69b1e1df01602728600cc74b60ff3ad
Ashwinijadi <ashwinijadisweety346@gmail.com>
2020-10-04 16:12:51 +0530

    added:Player to choose Letter O or X in TicTocToe game is added

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -1,5 +1,7 @@
 package com.capgemini.tictactoe_game;
 
+import java.util.Scanner;
+
 public class TicTacToeGame {
 	public void createBoard() {
 		char board[] = new char[10];
@@ -7,10 +9,19 @@ public class TicTacToeGame {
 			board[i] = '-';
 		}
 	}
-
+	public static void chooseLetter() {
+		System.out.println("Enter User letter O or X");
+		Scanner userinput = new Scanner(System.in);
+		char user = userinput.next().charAt(0);
+		System.out.println(user);
+		if (user == 'O' || user =='o') {
+			System.out.println("computers X");
+		} else {
+			System.out.println("computers O");
+		}
+	}
 	public static void main(String[] args) {
 		TicTacToeGame game = new TicTacToeGame();
 		game.createBoard();
-
-	}
+		chooseLetter();	}
 }
