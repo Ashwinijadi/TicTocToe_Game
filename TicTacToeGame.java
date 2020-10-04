d5a2690cb4413ebf23d5f2a0de8d677136b3988e
Ashwinijadi <ashwinijadisweety346@gmail.com>
2020-10-04 15:57:49 +0530

    added:Welcome to Tictoctoe game

---
+++ TicTacToeGame.java
@@ -0,0 +1,16 @@
+package com.capgemini.tictactoe_game;
+
+public class TicTacToeGame {
+	public void createBoard() {
+		char board[] = new char[10];
+		for (int i = 1; i < board.length; i++) {
+			board[i] = ' ';
+		}
+	}
+
+	public static void main(String[] args) {
+		TicTacToeGame game = new TicTacToeGame();
+		game.createBoard();
+
+	}
+}
