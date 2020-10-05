d3377c45b3095806e03c17868fc28a3cc02f4f20
Ashwinijadi <ashwinijadisweety346@gmail.com>
2020-10-05 13:52:12 +0530

    added:Computers play is added

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -5,9 +5,9 @@ import java.util.Scanner;
 
 public class TicTacToeGame {
 	private static Scanner userinput = new Scanner(System.in);
-	private static final int TAIL=0;
+	private static final int TAIL = 0;
 	private static final int HEAD = 1;
-	private static char usersChoice;;
+	private static char usersChoice;
 	private static char computer;
 	private static char[] board;
 
@@ -74,7 +74,7 @@ public class TicTacToeGame {
 			checkWinner(toss);
 		}
 	}
-	
+
 	// Check who is Winner or game is Tie
 	private static void checkWinner(int toss) {
 
@@ -94,7 +94,7 @@ public class TicTacToeGame {
 				|| (board[3] == board[5] && board[5] == board[7] && board[7] != ' ')) {
 			if (toss == HEAD)
 				System.out.println("Winner is User");
-			if (toss== TAIL)
+			if (toss == TAIL)
 				System.out.println("Winner is Computer");
 			System.exit(0);
 		} else {
@@ -115,14 +115,113 @@ public class TicTacToeGame {
 		displayBoard(board);
 	}
 
+	public static int computerPlaysWithBrain(char[] board, char choice) {
+		int index = 0;
+		while (index == 0) {
+			int i = (int) (Math.floor((Math.random() * 10) % 9) + 1);
+			if (spaceFree(board, i))
+				index = i;
+		}
+		if (choice == computer) {
+			if (((board[1] == board[2] && board[1] == computer) || (board[5] == board[7] && board[5] == computer)
+					|| (board[6] == board[9] && board[6] == computer)) && board[3] == ' ') {
+				index = 3;
+			} else if (((board[4] == board[5] && board[4] == computer)
+					|| (board[3] == board[9] && board[3] == computer)) && board[6] == ' ') {
+				index = 6;
+			} else if (((board[5] == board[1] && board[1] == computer) || (board[8] == board[7] && board[7] == computer)
+					|| (board[6] == board[3] && board[6] == computer)) && board[9] == ' ') {
+				index = 9;
+			} else if (((board[3] == board[2] && board[3] == computer) || (board[4] == board[7] && board[4] == computer)
+					|| (board[5] == board[9] && board[5] == computer)) && board[1] == ' ') {
+				index = 1;
+			} else if (((board[1] == board[7] && board[1] == computer)
+					|| (board[5] == board[6] && board[5] == computer)) && board[4] == ' ') {
+				index = 4;
+			} else if (((board[1] == board[4] && board[1] == computer) || (board[5] == board[3] && board[5] == computer)
+					|| (board[8] == board[9] && board[8] == computer)) && board[7] == ' ') {
+				index = 7;
+			} else if (((board[1] == board[3] && board[1] == computer)
+					|| (board[5] == board[8] && board[5] == computer)) && board[2] == ' ') {
+				index = 2;
+			} else if (((board[5] == board[2] && board[5] == computer)
+					|| (board[7] == board[9] && board[7] == computer)) && board[8] == ' ') {
+				index = 8;
+			} else if (((board[1] == board[9] && board[1] == computer) || (board[3] == board[7] && board[3] == computer)
+					|| (board[6] == board[4] && board[6] == computer)) && board[5] == ' ') {
+				index = 5;
+			} else if (((board[1] == board[2] && board[1] == usersChoice)
+					|| (board[5] == board[7] && board[5] == usersChoice)
+					|| (board[6] == board[9] && board[6] == usersChoice)) && board[3] == ' ') {
+				index = 3;
+			} else if (((board[4] == board[5] && board[4] == usersChoice)
+					|| (board[3] == board[9] && board[3] == usersChoice)) && board[6] == ' ') {
+				index = 6;
+			} else if (((board[5] == board[1] && board[1] == usersChoice)
+					|| (board[8] == board[7] && board[7] == usersChoice)
+					|| (board[6] == board[3] && board[6] == usersChoice)) && board[9] == ' ') {
+				index = 9;
+			} else if (((board[3] == board[2] && board[3] == usersChoice)
+					|| (board[4] == board[7] && board[4] == usersChoice)
+					|| (board[5] == board[9] && board[5] == usersChoice)) && board[1] == ' ') {
+				index = 1;
+			} else if (((board[1] == board[7] && board[1] == usersChoice)
+					|| (board[5] == board[6] && board[5] == usersChoice)) && board[4] == ' ') {
+				index = 4;
+			} else if (((board[1] == board[4] && board[1] == usersChoice)
+					|| (board[5] == board[3] && board[5] == usersChoice)
+					|| (board[8] == board[9] && board[8] == usersChoice)) && board[7] == ' ') {
+				index = 7;
+			} else if (((board[1] == board[3] && board[1] == usersChoice)
+					|| (board[5] == board[8] && board[5] == usersChoice)) && board[2] == ' ') {
+				index = 2;
+			} else if (((board[5] == board[2] && board[5] == usersChoice)
+					|| (board[7] == board[9] && board[7] == usersChoice)) && board[8] == ' ') {
+				index = 8;
+			} else if (((board[1] == board[9] && board[1] == usersChoice)
+					|| (board[3] == board[7] && board[3] == usersChoice)
+					|| (board[6] == board[4] && board[6] == usersChoice)) && board[5] == ' ') {
+				index = 5;
+			} else if (board[1] == ' ' && board[2] != ' ' && board[3] != ' ' && board[4] != ' ' && board[5] != ' '
+					&& board[6] != ' ' && board[7] != ' ' && board[8] != ' ' && board[9] != ' ') {
+				index = 1;
+			} else if (board[3] == ' ' && board[1] != ' ' && board[2] != ' ' && board[4] != ' ' && board[5] != ' '
+					&& board[6] != ' ' && board[7] != ' ' && board[8] != ' ' && board[9] != ' ') {
+				index = 3;
+			} else if (board[7] == ' ' && board[1] != ' ' && board[2] != ' ' && board[3] != ' ' && board[4] != ' '
+					&& board[5] != ' ' && board[6] != ' ' && board[8] != ' ' && board[9] != ' ') {
+				index = 7;
+			} else if (board[9] == ' ' && board[1] != ' ' && board[2] != ' ' && board[3] != ' ' && board[4] != ' '
+					&& board[5] != ' ' && board[6] != ' ' && board[7] != ' ' && board[8] != ' ') {
+				index = 9;
+			} else if (board[5] == ' ' && board[1] != ' ' && board[2] != ' ' && board[3] != ' ' && board[4] != ' '
+					&& board[6] != ' ' && board[7] != ' ' && board[8] != ' ' && board[9] != ' ') {
+				index = 5;
+			} else if (board[2] == ' ' && board[1] != ' ' && board[3] != ' ' && board[4] != ' ' && board[5] != ' '
+					&& board[6] != ' ' && board[7] != ' ' && board[8] != ' ' && board[9] != ' ') {
+				index = 2;
+			} else if (board[4] == ' ' && board[1] != ' ' && board[2] != ' ' && board[3] != ' ' && board[5] != ' '
+					&& board[6] != ' ' && board[7] != ' ' && board[8] != ' ' && board[9] != ' ') {
+				index = 4;
+			} else if (board[6] == ' ' && board[1] != ' ' && board[2] != ' ' && board[3] != ' ' && board[4] != ' '
+					&& board[5] != ' ' && board[7] != ' ' && board[8] != ' ' && board[9] != ' ') {
+				index = 6;
+			} else if (board[8] == ' ' && board[1] != ' ' && board[2] != ' ' && board[3] != ' ' && board[4] != ' '
+					&& board[5] != ' ' && board[6] != ' ' && board[7] != ' ' && board[9] != ' ') {
+				index = 8;
+			}
+		}
+		return index;
+	}
+
 	public static void main(String[] args) {
 		char board[] = new char[10];
 		TicTacToeGame game = new TicTacToeGame();
 		game.createBoard();
-		char userLetter = chooseLetter();
+		usersChoice = chooseLetter();
 		TicTacToeGame.displayBoard(board);
 		int userMove = locationSelection(board);
-		makeMove(board, userLetter);
+		makeMove(board, usersChoice);
 		game.checkToss();
 	}
 }
